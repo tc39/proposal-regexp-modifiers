19e6915854558736cbe3235f2640ab1fe3f557c3
Ron Buckton <ron.buckton@microsoft.com>
2022-05-26 15:26:35 -0700

    Update base spec text

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,37 @@
 const del = require("del");
+const path = require("path");
 const gulp = require("gulp");
 const emu = require("gulp-emu");
+const rename = require("gulp-rename");
 const gls = require("gulp-live-server");
 
-const clean = () => del("docs/**/*");
-gulp.task("clean", clean);
+gulp.task("clean", () => del("docs/**/*"));
 
-const build = () => gulp
-    .src(["spec/index.html"])
-    .pipe(emu())
-    .pipe(gulp.dest("docs"));
-gulp.task("build", build);
+gulp.task("build", () => gulp
+    .src(["spec.emu"])
+    .pipe(emu({
+        log: require("ecmarkup/lib/utils").logVerbose,
+        warn: err => {
+            const file = path.resolve(err.file || "spec.emu");
+            const message = `Warning: ${file}:${typeof err.line === "number" ? `${err.line}:${err.column}:` : ""} ${err.message}`;
+            require("ecmarkup/lib/utils").logWarning(message);
+        },
+        ecma262Biblio: false,
+    }))
+    .pipe(rename("index.html"))
+    .pipe(gulp.dest("docs")));
 
-const watch = () => gulp
-    .watch(["spec/**/*"], build);
-gulp.task("watch", watch);
+gulp.task("watch", () => gulp
+    .watch(["spec.emu"], gulp.task("build")));
 
-const start = () => {
+gulp.task("start", gulp.parallel("watch", () => {
     const server = gls.static("docs", 8080);
     const promise = server.start();
-    gulp.watch(["docs/**/*"], file => server.notify(file));
+    (/** @type {import("chokidar").FSWatcher}*/(gulp.watch(["docs/**/*"])))
+        .on("change", file => {
+            server.notify({ path: path.resolve(file) });
+        });
     return promise;
-};
-gulp.task("start", gulp.parallel(watch, start));
+}));
 
-gulp.task("default", build);
+gulp.task("default", gulp.task("build"));
